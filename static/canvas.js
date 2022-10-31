f9ef8cb4bc0745a5967a1daea7391a4da8bb4f03
Pramit Bhatia <pramitbhatia25@gmail.com>
2022-10-31 17:05:22 -0400

    Reset

--- static/canvas.js
+++ static/canvas.js
@@ -280,7 +280,7 @@ function animate() {
   else {
     p.velocity.x = 0;
     if (keys.right.pressed && won == false) {
-      score += 2;
+      score += 1;
       scrollOfset += 10;
       platforms.forEach((pl) => {
         pl.position.x -= 10;
